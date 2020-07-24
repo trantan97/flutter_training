143e9b960791a8f8af0775da5e3a2a3fa27dcbd2
Tran Tan <tanmhtb@gmail.com>
2020-07-24 22:37:57 +0700

    ui excercise

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'doctor_detail.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: DetailDoctorScreen(),
+  ));
+}
---
+++ lib/my_images.dart
@@ -0,0 +1,4 @@
+class MyImages{
+  static final avatar = "assets/images/avatar.png";
+  static final address = "assets/images/address.png";
+}
